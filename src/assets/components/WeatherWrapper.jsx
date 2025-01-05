ed4131f7fd04efe53b22b7d4dc6af2145dd55822
otizgit <emmanueljoe777@gmail.com>
2025-01-05 23:20:23 +0100

    google map init

--- src/assets/components/WeatherWrapper.jsx
+++ src/assets/components/WeatherWrapper.jsx
@@ -12,6 +12,7 @@ export default function WeatherWrapper({
   changeLanguage,
 }) {
   const { lat, lon } = weather.coord;
+  const zoom = 15;
 
   return (
     <div className="container min-h-screen max-w-[100vw] relative py-[3rem]">
@@ -23,7 +24,20 @@ export default function WeatherWrapper({
           language={language}
           changeLanguage={changeLanguage}
         />
-        <WeatherMainInfo weather={weather} />
+        <div className="grid-style gap-2">
+          <WeatherMainInfo weather={weather} />
+          <div className="glass border-style">
+            <iframe
+              width="600"
+              height="250"
+              style={{ border: "none" }}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              src={`https://maps.google.com/maps?q=${lat},${lon}&z=${zoom}&output=embed`}
+              title="google map"
+            ></iframe>
+          </div>
+        </div>
         {/* <LocationMap latitude={lat} longitude={lon} /> */}
       </div>
     </div>
