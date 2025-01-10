42bd3ad9855add4f1c825a3a999e29af65a31d41
otizgit <emmanueljoe777@gmail.com>
2025-01-10 21:50:27 +0100

    question mark addition

--- src/assets/components/ErrorPage.jsx
+++ src/assets/components/ErrorPage.jsx
@@ -5,11 +5,14 @@ export default function ErrorPage() {
     <div>
       <div className="flex flex-col items-center">
         <h1 className="mb-2">Hmmm, I can't find that location.</h1>
-        <img
-          className="w-[250px] sm:w-[300px] opacity-85"
-          src="https://ik.imagekit.io/uldpul7gs/tr:w-1200/earth.png?updatedAt=1736541194734"
-          alt="image of earth"
-        />
+        <div className="relative">
+            <p className="absolute text-[3rem] rotate-12 right-10 z-20">❓</p>
+          <img
+            className="w-[250px] sm:w-[300px] opacity-85"
+            src="https://ik.imagekit.io/uldpul7gs/tr:w-1200/earth.png?updatedAt=1736541194734"
+            alt="image of earth"
+          />
+        </div>
       </div>
     </div>
   );
