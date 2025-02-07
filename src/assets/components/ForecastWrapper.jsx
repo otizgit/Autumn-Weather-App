be8082e911679bfeea379b38087f12dd35196e25
otizgit <emmanueljoe777@gmail.com>
2025-02-07 20:58:04 +0100

    height fit removal

--- src/assets/components/ForecastWrapper.jsx
+++ src/assets/components/ForecastWrapper.jsx
@@ -4,7 +4,7 @@ import sun from "../img/sun.png";
 
 export default function ForecastWrapper({ unit, dailyForecast, forecast }) {
   return (
-    <div className="flex-1 flex flex-col h-fit">
+    <div className="flex-1 flex flex-col">
       <div className="border-style glass p-5 rounded-xl custom-fz mb-4">
         <h1 className="font-semibold mb-4 custom-fz">Hourly Forecast</h1>
         <div className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-10 gap-4 items-center">
