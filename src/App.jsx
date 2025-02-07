adad2cde79801a9c8c28afe3bb0d3c2e3fe700cf
otizgit <emmanueljoe777@gmail.com>
2025-02-07 23:07:30 +0100

    lenis scroll and bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Lenis from "lenis";
 import WeatherWrapper from "./assets/components/WeatherWrapper";
 
 export default function App() {
+  const lenis = new Lenis({
+    autoRaf: true,
+  });
+
   return (
     <div className="container min-h-screen max-w-[100vw] relative py-[3rem]">
       <WeatherWrapper />
